1ac6f0737ac941594622b9692d4efedb41528158
ajurasz <arkadiusz.jurasz@gmail.com>
2017-05-04 13:02:32 +0200

    Fixed production configuration.

--- client/src/store/configureStore.prod.js
+++ client/src/store/configureStore.prod.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import thunk from 'redux-thunk';
+
+import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
     return createStore(
